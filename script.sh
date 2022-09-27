db67d3549f347a712046c9f5b6f300574b4e85d1
Guto Ribeiro <gutoinfo.ribeiro@gmail.com>
2022-09-26 23:02:39 -0300

    feat:adiciona script para automação de config de server

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#Criando diretórios padrões
+echo "[+] Criando diretórios a partir dos nomes no dirs.txt"
+
+for dir_name in $(cat dirs.txt); do
+    echo "Criando diretório ${dir_name}"
+    mkdir $dir_name
+done
+
+#Criando grupos
+echo "[+] Criando grupos a partir dos nomes no groups.txt"
+
+for group_name in $(cat groups.txt); do
+    echo "Criando grupo ${group_name}"
+    groupadd $group_name
+done
+
+#Criando usuários e adicionando aos respectivos grupos
+echo "[+] Gerando comandos de criação de usuários..."
+awk '{print "useradd " $1 " -m -s /bin/bash -G " $2}' usersXgroups.txt > commands.sh
+
+echo "[+] Criando usuários adicionando aos grupos a partir dos comandos gerados no passo anterior"
+sed -i '1 i\#!/usr/bin/env bash \n' commands.sh
+chmod +x commands.sh
+./commands.sh
+
+PASSWORD="Senha123"
+for user in $(awk '{print $1}' usersXgroups.txt); do
+    echo "[+] Alterando a senha de $user para a senha padrão"
+    echo "$user:$PASSWORD" | chpasswd
+    echo "  [->] Forçar $user trocar a senha no primeiro login "
+    chage -d 0 $user
+done
